70f59aa8c2242e996c4329a2e3cc9a7fca8d4ef9
Marijn Suijten <marijns95@gmail.com>
2025-08-17 18:40:00 +0200

    Practice a second instance launch (not even in split window)

--- examples/examples/looper.rs
+++ examples/examples/looper.rs
@@ -1,16 +1,25 @@
 //! Demonstrates how to manage application lifetime using Android's `Looper`
 
-use log::info;
+use android_intent::{Action, Intent};
+use log::{error, info};
 
-// Don't use ndk-glue/ndk-macro startup for now.
 #[no_mangle]
 unsafe extern "C" fn ANativeActivity_onCreate(app: *mut ndk_sys::ANativeActivity) {
     android_logger::init_once(
-        android_logger::Config::default().with_max_level(log::LevelFilter::Trace),
+        android_logger::Config::default().with_max_level(log::LevelFilter::Info),
     );
+    error!("LAUNCHED {app:?}");
     ndk_glue::init(app, std::ptr::null_mut(), 0, our_main);
 }
 
 fn our_main() {
     info!("HELLO!");
+
+    // Bad hack but nice for quick iteration
+    android_intent::with_current_env(|env| {
+        let x = Intent::new(env, Action::Main);
+        x.set_class_name("rust.example.looper", "android.app.NativeActivity")
+            .start_activity()
+            .unwrap()
+    })
 }
